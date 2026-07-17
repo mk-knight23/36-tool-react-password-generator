51916ad85cd2a8eea687bc098964d1386bf7ea86
kazi-reprime <kazi@reprime.com>
2026-07-17 15:47:46 +0530

    chore: scaffold Next.js 16 App Router + Tailwind v4, purge legacy Vue/Vite app and evolution bots

--- .github/metadata_update.sh
+++
@@ -1,4 +0,0 @@
-gh repo edit mk-knight23/36-tool-react-password-generator \
-  --description "Modern web application built with Node.js, Vite, HTML/CSS/JS | Part of 60 Projects Ecosystem" \
-  --website "https://mk-knight23.github.io/36-tool-react-password-generator" \
-  --add-topic web-application,javascript,frontend
